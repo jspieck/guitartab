a14c4040c76c1af9bf60094b81429a4b5558c70a
Jan Spieck <jspnbg@gmail.com>
2023-04-12 17:30:29 +0200

    TimeMeterModal

--- src/App.vue
+++ src/App.vue
@@ -7,6 +7,7 @@ import Equalizer from './components/Equalizer.vue'
 import Sequencer from './components/Sequencer.vue'
 import GuitarModal from './components/GuitarModal.vue'
 import PianoModal from './components/PianoModal.vue'
+import TimeMeterModal from './components/TimeMeterModal.vue'
 import { startUp } from './assets/js/guitarTab'
 
 const equalizer = ref(null);
@@ -337,76 +338,7 @@ onMounted(() => {
       </div>
     </div>
 
-    <div id="timeMeterModal" class="modal" role="alert">
-      <div class="modalTopBar">
-        <label class="modalTopBarLabel">Time Meter</label>
-        <div class="modal_close">
-          <div class="icon">
-            <svg viewBox="0 0 32 32">
-              <use xlink:href="#close-icon"></use>
-            </svg>
-          </div>
-        </div>
-      </div>
-      <div class="modalBody">
-        <div class="timeMeterSelectCapsule">
-          <label class="labelTopMargin">Numerator</label>
-          <div class="select">
-            <select id="numeratorSelection">
-              <option value="1">1</option>
-              <option value="2">2</option>
-              <option value="3">3</option>
-              <option value="4" selected>4</option>
-              <option value="5">5</option>
-              <option value="6">6</option>
-              <option value="7">7</option>
-              <option value="8">8</option>
-              <option value="9">9</option>
-              <option value="10">10</option>
-              <option value="11">11</option>
-              <option value="12">12</option>
-              <option value="13">13</option>
-              <option value="14">14</option>
-              <option value="15">15</option>
-              <option value="16">16</option>
-              <option value="17">17</option>
-              <option value="18">18</option>
-              <option value="19">19</option>
-              <option value="20">20</option>
-              <option value="21">21</option>
-              <option value="22">22</option>
-              <option value="23">23</option>
-              <option value="24">24</option>
-              <option value="25">25</option>
-              <option value="26">26</option>
-              <option value="27">27</option>
-              <option value="28">28</option>
-              <option value="29">29</option>
-              <option value="30">30</option>
-              <option value="31">31</option>
-            </select>
-            <div class="select__arrow"></div>
-          </div>
-          <label class="labelTopMargin">Denominator</label>
-          <div class="select timeMeterSelect">
-            <select id="denominatorSelection">
-              <option value="1">1</option>
-              <option value="2">2</option>
-              <option value="4" selected>4</option>
-              <option value="8">8</option>
-              <option value="16">16</option>
-              <option value="32">32</option>
-            </select>
-            <div class="select__arrow"></div>
-          </div>
-        </div>
-        <svg id="timeMeterSelectButton" class="checkmark selectButton" xmlns="http://www.w3.org/2000/svg"
-          viewBox="0 0 52 52">
-          <circle class="checkmark__circle" cx="26" cy="26" r="25" fill="none" />
-          <path class="checkmark__check" fill="none" d="M14.1 27.2l7.1 7.2 16.7-16.8" />
-        </svg>
-      </div>
-    </div>
+    <TimeMeterModal id="timeMeterModal" class="modal" role="alert"/>
 
     <div id="bpmModal" class="modal" role="alert">
       <div class="modalTopBar">
---
+++ src/components/TimeMeterModal.vue
@@ -0,0 +1,55 @@
+<template>
+    <div id="timeMeterModal" class="modal" role="alert">
+      <div class="modalTopBar">
+        <label class="modalTopBarLabel">Time Meter</label>
+        <div class="modal_close">
+          <div class="icon">
+            <svg viewBox="0 0 32 32">
+              <use xlink:href="#close-icon"></use>
+            </svg>
+          </div>
+        </div>
+      </div>
+      <div class="modalBody">
+        <div class="timeMeterSelectCapsule">
+          <label class="labelTopMargin">Numerator</label>
+          <div class="select">
+            <select v-model="numerator">
+              <option v-for="n in 31" :key="n" :value="n">{{ n }}</option>
+            </select>
+            <div class="select__arrow"></div>
+          </div>
+          <label class="labelTopMargin">Denominator</label>
+          <div class="select timeMeterSelect">
+            <select v-model="denominator">
+              <option v-for="n in denominators" :key="n" :value="n">{{ n }}</option>
+            </select>
+            <div class="select__arrow"></div>
+          </div>
+        </div>
+        <svg
+          id="timeMeterSelectButton"
+          class="checkmark selectButton"
+          xmlns="http://www.w3.org/2000/svg"
+          viewBox="0 0 52 52"
+          @click="onSelectButtonClick"
+        >
+          <circle class="checkmark__circle" cx="26" cy="26" r="25" fill="none" />
+          <path class="checkmark__check" fill="none" d="M14.1 27.2l7.1 7.2 16.7-16.8" />
+        </svg>
+      </div>
+    </div>
+  </template>
+  
+  <script setup lang="ts">
+  import { ref } from 'vue';
+  
+  const numerator = ref(4);
+  const denominator = ref(4);
+  const denominators = [1, 2, 4, 8, 16, 32];
+  
+  function onSelectButtonClick() {
+    console.log('Selected time meter:', numerator.value, denominator.value);
+  }
+  </script>
+  
